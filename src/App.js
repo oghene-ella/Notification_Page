32f8a5a5815a5216f340168c16eb01e0dfaf57cb
oghene-ella <oghenekaro57@gmail.com>
2023-12-02 10:39:39 +0100

    project structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles/local.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-red-600 text-5xl text-white">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
